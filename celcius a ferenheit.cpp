ecfee232a3a8de18227b48b03f3f6dd6fb4b7b19
eliascando <75767835+eliascando@users.noreply.github.com>
2021-01-10 16:23:26 -0500

    Update celcius a ferenheit.cpp
    Added do while statments

--- celcius a ferenheit.cpp
+++ celcius a ferenheit.cpp
@@ -1,35 +1,51 @@
 /*Esta es un programa que convierte de farenheit a celcius y visceversa*/
-
+/*Elias Cando*/
 #include <stdio.h>
 #include <conio.h>
 #include <locale.h>
 
 main () {
 	setlocale(LC_ALL, "spanish");
-	int select, celcius, farenheit=0;
-	int cont=1;
+	float celcius, farenheit=0;
+	int i, select;
+	
+	do
+	{	
+	fflush(stdin);
 	printf("\n ****Convertidor de temperatura**** ");
 	printf("\n 1. Celcius a Farenheit.");
 	printf("\n 2. Farenheit a Celcius.");
-	printf("\n Eliga opcion: ");
+	printf("\n Elija opcion: ");
 	scanf ("%i",&select);
-	switch (select){
+		switch (select)
+		{
 		case 1:
 			printf("Ingrese Grados Celcius: ");
-			scanf ("%i",&celcius);
+			scanf ("%f",&celcius);
 			farenheit = (celcius * 1.8) + 32;
-			printf("\n Es igual a: %i", farenheit);
-			printf("캟");
-			break;
+			printf("\n Es igual a: %.2f", farenheit);
+			printf("째F");
+		break;
+		
 		case 2:
 			printf("Ingrese Grados Farenheit: ");
-			scanf ("%i",&farenheit);
+			scanf ("%f",&farenheit);
 			celcius = (farenheit - 32) / 1.8;
-			printf("\n Es igual a: %i", celcius);
-			printf("캜");
-			break;
+			printf("\n Es igual a: %.2f", celcius);
+			printf("째C");
+		break;
+		
 		default:
-			printf("\n Opcion Incorrecta...");
-	}
+			printf("\n Opci처n Incorrecta...");
+		}
+				printf("\n \n ***********************");
+				printf("\n \n Desea volver a Convertir?...");
+				printf("\n 1. SI");
+				printf("\n 2. NO");
+				printf("\n Opci처n: ");
+				scanf ("%i",&i);
+	} while (i == 1);	
+	printf("\n ****PROGRAMA FINALIZADO****");
+	
 	getch();
 }
